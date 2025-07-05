55231ecce91b872317f2118cabe033b4cc41f465
imattking <matt@imattking.com>
2025-07-05 13:29:07 -0400

    feat: add journal entry detail page to the FE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router';
 import HomePage from './pages/HomePage';
 import SignInPage from './pages/SignInPage';
 import JournalPage from './pages/JournalPage';
+import JournalEntryDetailPage from './pages/JournalEntryDetailPage';
 import MarketPage from './pages/MarketPage';
 import SettingsPage from './pages/SettingsPage';
 
@@ -12,6 +13,8 @@ const App = () => {
 				<Route path="/" element={<HomePage />} />
 				<Route path="/signin" element={<SignInPage />} />
 				<Route path="/journal" element={<JournalPage />} />
+				{/*Path for the Journal Entry Route will need to be updated, once we get the endpoint setup from the Backend*/}
+				<Route path="/journal-entry" element={<JournalEntryDetailPage />} />
 				<Route path="/market" element={<MarketPage />} />
 				<Route path="/settings" element={<SettingsPage />} />
 			</Routes>
