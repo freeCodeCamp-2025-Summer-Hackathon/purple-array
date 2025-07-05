00da2d7d326aaaeb9a9361a0e08b8088e9639491
Victor Wong <victorwong315@gmail.com>
2025-07-05 17:07:25 -0400

    feat: added word of the day component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from '../components/generic/Navbar';
-
+import WordOfTheDay from '../components/WordOfTheDay';
 const HomePage = () => {
 	return (
 		<div className="min-h-screen">
 			<Navbar />
 			<h1>Home Page</h1>
+			<WordOfTheDay />
 		</div>
 	);
 };
