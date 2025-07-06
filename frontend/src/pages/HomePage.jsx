eb8bcc9caf91608ef1e5b2761a2d511a58cb1257
Victor Wong <victorwong315@gmail.com>
2025-07-05 20:37:33 -0400

    feat: created Home folder and moved wordOfTheDay file into it

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from '../components/generic/Navbar';
-import WordOfTheDay from '../components/WordOfTheDay';
+import WordOfTheDay from '../components/Home/WordOfTheDay';
 const HomePage = () => {
 	return (
 		<div className="min-h-screen">
 			<Navbar />
 			<h1>Home Page</h1>
 			<WordOfTheDay />
+
 		</div>
 	);
 };
