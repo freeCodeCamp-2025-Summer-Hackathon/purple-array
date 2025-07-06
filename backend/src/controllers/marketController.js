faad6d19feca82569043ff8143d6dbde06f4046d
haednodik <haednodik@gmail.com>
2025-07-06 17:48:58 -0400

    feat: update return as json with items in an array

--- backend/src/controllers/marketController.js
+++ backend/src/controllers/marketController.js
@@ -3,7 +3,7 @@ import Market from '../models/Market.js';
 export async function getAllMarkets(_, res) {
     try {
         const markets = await Market.find().sort({ createdAt: -1 });
-        res.status(200).json(markets);
+        res.status(200).json({ items: markets });
     } catch (error) {
         console.error('Error in getAllMarkets controller', error);
         res.status(500).json({ message: 'Internal server error' });
