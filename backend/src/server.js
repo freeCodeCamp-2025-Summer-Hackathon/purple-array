79d009f6112f04f0017b519b023cbc7bca8e2403
haednodik <haednodik@gmail.com>
2025-07-06 17:35:03 -0400

    feat: update server.js to handle market routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 
+import marketsRoutes from './routes/marketsRoutes.js';
+
 dotenv.config();
 
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
+
+// middleware
+app.use(express.json());
+
+app.use('/markets', marketsRoutes);
 
 app.listen(PORT, () => {
-    console.log('Server started on PORT:', PORT)
+    console.log('Server started on PORT:', PORT);
 });
