b21b9d93b518ee384fccf4566a898bd9ebd10604
Matthew <106106690+imattking@users.noreply.github.com>
2025-07-06 16:22:38 -0400

    Update JournalEntryDetailPage.jsx
    - update name of import
    - update name in component instance

--- frontend/src/pages/JournalEntryDetailPage.jsx
+++ frontend/src/pages/JournalEntryDetailPage.jsx
@@ -1,12 +1,12 @@
 import Navbar from '../components/generic/Navbar';
-import JournalDetail from '../components/journal/JournalDetail';
+import JournalDetail from '../components/journal/JournalEntryDetail';
 
 const JournalEntryDetailPage = () => {
 	return (
 		<div className="min-h-screen">
 			<Navbar />
 			<h1>Journal Entry Detail Page</h1>
-            <JournalDetail />
+            <JournalEntryDetail />
 		</div>
 	);
 };
