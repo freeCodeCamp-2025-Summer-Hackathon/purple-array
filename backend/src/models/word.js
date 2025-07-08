0d4965655135cf6609072d0772443b09191d3eec
Damian <mystelaurora@gmail.com>
2025-07-08 18:55:59 -0400

    feat: update Word naming conventions

--- backend/src/models/word.js
+++ backend/src/models/word.js
@@ -1,31 +1,30 @@
-const mongoose = require('mongoose')
+import mongoose from 'mongoose';
 
-
-const WordSchema = new mongoose.Schema({
-    word:{
-        type:String,
-        required: [true, "please provide a word"],
-        minlength:2 ,
+const wordSchema = new mongoose.Schema({
+    word: {
+        type: String,
+        required: [true, 'please provide a word'],
+        minlength: 2,
     },
-    pronunciation:{
-        type:String,
-        required: [true, "please provide the pronunciation"],
-        minlength:2 ,
+    pronunciation: {
+        type: String,
+        required: [true, 'please provide the pronunciation'],
+        minlength: 2,
     },
-    definition:{
-        type:String,
-        required: [true, "please provide the definition"],
-        minlength:2 ,
+    definition: {
+        type: String,
+        required: [true, 'please provide the definition'],
+        minlength: 2,
     },
-    lastUsed:{
-        type:Date,
-        
+    lastUsed: {
+        type: Date,
     },
-    history:{
-        type:[Date],
-        default:[]
-    }
-
+    history: {
+        type: [Date],
+        default: [],
+    },
 });
 
-module.exports = mongoose.model('Word', WordSchema)
+const Word = mongoose.model('Word', wordSchema);
+
+export default Word;
