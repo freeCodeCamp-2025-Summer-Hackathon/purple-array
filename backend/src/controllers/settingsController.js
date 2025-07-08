07bd0efe5e5d618f315ac21115c7c400b663edc5
Damian <mystelaurora@gmail.com>
2025-07-08 01:23:51 -0400

    feat: add GET settings endpoint

---
+++ backend/src/controllers/settingsController.js
@@ -0,0 +1,13 @@
+import User from '../models/User.js';
+
+export async function getAllSettings(req, res) {
+    try {
+        const settings = await User.findById(req.params.id, 'settings -_id'); // returns only settings field, removes _id
+        if (!settings)
+            return res.status(404).json({ message: 'Settings not found' });
+        res.status(200).json(settings);
+    } catch (error) {
+        console.error('Error in getAllSettings controller', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 
 import productsRoutes from './routes/productsRoutes.js';
+import settingsRoutes from './routes/settingsRoutes.js';
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json());
 
 app.use('/products', productsRoutes);
+app.use('/settings', settingsRoutes);
 
 app.listen(PORT, () => {
     console.log('Server started on PORT:', PORT);
