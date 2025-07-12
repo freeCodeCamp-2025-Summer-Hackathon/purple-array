051915ae2d23e075a606c07d1f94c35bef0f8b1e
haednodik <haednodik@gmail.com>
2025-07-12 18:57:59 -0400

    feat: add wordHelper for word staging with simple daily rotation

--- backend/src/helpers/wordHelper.js
+++ backend/src/helpers/wordHelper.js
@@ -1,8 +1,54 @@
-import Word from '../models/Word.js';
+import { Word, WordOfTheDay } from '../models/Word.js';
+import schedule from 'node-schedule';
 
-export const setWord = async () => {
-    const words = await Word.find();
-    const { word, pronunciation, definition } =
-        words[Math.floor(Math.random() * words.length)];
-    return { word, pronunciation, definition };
+export const wordObj = {
+    previousWord: {},
+    currentWord: {},
+    nextWord: {},
 };
+
+const setWord = async () => {
+    const word = await Word.aggregate([
+        {
+            $match: { word: { $ne: wordObj.previousWord.word } },
+        },
+        {
+            $match: { word: { $ne: wordObj.currentWord.word } },
+        },
+    ]).sample(1);
+    return word[0];
+};
+
+// example of executing the start of every day in the UTC timezone
+// default value of a component is null
+// if minute not explicitly set to 0, rule will run every minute
+const rule = new schedule.RecurrenceRule();
+rule.hour = 0;
+rule.minute = 0;
+rule.tz = 'Etc/UTC';
+
+let running = false;
+const _job = schedule.scheduleJob({ rule }, async function () {
+    console.log('A new day has begun in the UTC timezone!');
+    if (running === false) {
+        wordObj.previousWord = await Word.findOne()
+            .sort({ createdAt: 1 })
+            .exec();
+        wordObj.currentWord = await Word.findOne()
+            .sort({ createdAt: 1 })
+            .exec();
+        wordObj.nextWord = await setWord();
+        running = true;
+    }
+    wordObj.previousWord = wordObj.currentWord;
+    wordObj.currentWord = wordObj.nextWord;
+    wordObj.nextWord = await setWord();
+
+    // wordObj handles word swaps but for persistent data, need to add to database
+    const newWord = new WordOfTheDay({
+        previousWord: wordObj.previousWord,
+        currentWord: wordObj.currentWord,
+        nextWord: wordObj.nextWord,
+    });
+    const _savedWord = await newWord.save();
+});
--- backend/src/models/Word.js
+++ backend/src/models/Word.js
@@ -1,30 +1,45 @@
 import mongoose from 'mongoose';
 
-const wordSchema = new mongoose.Schema({
-    word: {
-        type: String,
-        required: [true, 'please provide a word'],
-        minlength: 2,
+const wordSchema = new mongoose.Schema(
+    {
+        word: {
+            type: String,
+            required: [true, 'please provide a word'],
+            minlength: 2,
+        },
+        pronunciation: {
+            type: String,
+            required: [true, 'please provide the pronunciation'],
+            minlength: 2,
+        },
+        definition: {
+            type: String,
+            required: [true, 'please provide the definition'],
+            minlength: 2,
+        },
+        lastUsed: {
+            type: Date,
+        },
+        history: [
+            {
+                id: String,
+                lastUsed: Date,
+            },
+        ],
     },
-    pronunciation: {
-        type: String,
-        required: [true, 'please provide the pronunciation'],
-        minlength: 2,
-    },
-    definition: {
-        type: String,
-        required: [true, 'please provide the definition'],
-        minlength: 2,
-    },
-    lastUsed: {
-        type: Date,
-    },
-    history: {
-        type: [Date],
-        default: [],
-    },
-});
+    { timestamps: true, minimize: false }
+);
 
-const Word = mongoose.model('Word', wordSchema);
+const wordOfTheDaySchema = new mongoose.Schema(
+    {
+        previousWord: wordSchema,
+        currentWord: wordSchema,
+        nextWord: wordSchema,
+    },
+    { timestamps: true, minimize: false }
+);
 
-export default Word;
+export const Word = mongoose.model('Word', wordSchema);
+export const WordOfTheDay = mongoose.model('WordOfTheDay', wordOfTheDaySchema);
+
+export default { Word, WordOfTheDay };
