2550b44b35fa139f7e8e14e8fc1b6ffe6fe394dd
haednodik <haednodik@gmail.com>
2025-07-12 19:49:24 -0400

    feat: add persistence with new schema

--- backend/src/helpers/wordHelper.js
+++ backend/src/helpers/wordHelper.js
@@ -23,32 +23,31 @@ const setWord = async () => {
 // default value of a component is null
 // if minute not explicitly set to 0, rule will run every minute
 const rule = new schedule.RecurrenceRule();
-rule.hour = 0;
-rule.minute = 0;
+//rule.hour = 0;
+//rule.minute = 0;
 rule.tz = 'Etc/UTC';
 
 let running = false;
 const _job = schedule.scheduleJob({ rule }, async function () {
-    console.log('A new day has begun in the UTC timezone!');
     if (running === false) {
-        wordObj.previousWord = await Word.findOne()
-            .sort({ createdAt: 1 })
+        const wordOfTheDay = await WordOfTheDay.findOne()
+            .sort({ createdAt: -1 })
             .exec();
-        wordObj.currentWord = await Word.findOne()
-            .sort({ createdAt: 1 })
-            .exec();
-        wordObj.nextWord = await setWord();
-        running = true;
-    }
-    wordObj.previousWord = wordObj.currentWord;
-    wordObj.currentWord = wordObj.nextWord;
-    wordObj.nextWord = await setWord();
 
-    // wordObj handles word swaps but for persistent data, need to add to database
-    const newWord = new WordOfTheDay({
-        previousWord: wordObj.previousWord,
-        currentWord: wordObj.currentWord,
-        nextWord: wordObj.nextWord,
-    });
-    const _savedWord = await newWord.save();
+        wordObj.previousWord = wordOfTheDay.previousWord;
+        wordObj.currentWord = wordOfTheDay.currentWord;
+        wordObj.nextWord = wordOfTheDay.nextWord;
+        running = true;
+    } else {
+        wordObj.previousWord = wordObj.currentWord;
+        wordObj.currentWord = wordObj.nextWord;
+        wordObj.nextWord = await setWord();
+
+        const newWord = new WordOfTheDay({
+            previousWord: wordObj.previousWord,
+            currentWord: wordObj.currentWord,
+            nextWord: wordObj.nextWord,
+        });
+        const _savedWord = await newWord.save();
+    }
 });
