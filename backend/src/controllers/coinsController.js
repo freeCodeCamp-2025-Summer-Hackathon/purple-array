37198f4986d623a7183f9f91653bd0ca04715df6
Avi161 <paudela@union.edu>
2025-07-11 22:01:23 -0400

    feat: add get coins method

--- backend/src/controllers/coinsController.js
+++ backend/src/controllers/coinsController.js
@@ -2,7 +2,7 @@ import User from '../models/User.js';
 
 export async function getAllCoins(req, res) {
     try {
-        const coins = await User.find();
+        const coins = await User.findById(req.id, 'coins -_id');
         res.status(200).json({ coins });
     } catch (error) {
         console.error('Error in getAll Coins controller', error);
