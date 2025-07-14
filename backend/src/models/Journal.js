1c744aeafdfd7c07909b164bc7d1ef5f1e3762ed
Dark5888 <liu.mike588@gmail.com>
2025-07-14 14:52:59 -0400

    final Journal Schema

--- backend/src/models/Journal.js
+++ backend/src/models/Journal.js
@@ -6,10 +6,24 @@ const journalSchema = new mongoose.Schema({
         default: '',
     },
     period: {
-        type: Date,
+        type: String,
+        default: '',
     },
-    history: {
-        type: [mongoose.Schema.Types.ObjectId],
+    journal: {
+        type: [
+            {
+                // stores the current month's entries in date order
+                date: String, // year-month-date, e.g. 2025-07-05
+                word: String,
+                response: String,
+                optionalPrompt1: String,
+                response1: String,
+                optionalPrompt2: String,
+                response2: String,
+                optionalPrompt3: String,
+                response3: String,
+            },
+        ],
     },
 });
 
