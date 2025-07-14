dc3eed40e8b97dde65f031fda877cf49f9759124
Dark5888 <liu.mike588@gmail.com>
2025-07-14 14:58:32 -0400

    final journal schema

--- backend/src/controllers/journalController.js
+++
@@ -1,5 +0,0 @@
-//import Journal from '../models/Journal';
-
-export async function getJournal(req, res) {
-    res.send('Get Journal endpoint');
-}
--- backend/src/routes/journalRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-import { getJournal } from '../controllers/journalController.js';
-const router = express.Router();
-
-router.route('/').get(getJournal);
-
-export default router;
