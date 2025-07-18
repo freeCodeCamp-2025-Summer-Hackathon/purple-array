3f9eceed67daa48a821a9aa44324d7722583e8b7
imattking <matt@imattking.com>
2025-07-17 21:58:24 -0400

    fix [refactor]: fix bugs and tweak navbar styles

--- frontend/src/components/generic/Navbar.jsx
+++ frontend/src/components/generic/Navbar.jsx
@@ -1,109 +1,109 @@
-import toast from "react-hot-toast";
-import { Routes, Route, NavLink } from "react-router-dom";
-import { Sparkles, NotebookPen, Store, Cog } from "lucide-react";
+import toast from 'react-hot-toast';
+import { NavLink } from 'react-router';
+import { Sparkles, NotebookPen, Store, Cog } from 'lucide-react';
 
 const Navbar = () => {
-  return (
-    <div>
-      <header className="bg-base-200 border-b border-base-content/10">
-        <div className="mx-auto max-w-7xl p-4">
-          <div className="flex items-center justify-between">
-            {/* Logo & Site Title  */}
-            <div className="flex gap-4">
-              <NavLink
-                to={"/"}
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-2xl bg-gradient-to-b from-primary/10 to-transparent text-primary px-4 py-2 rounded-t-xl"
-                    : "text-2xl font-semibold text-secondary"
-                }
-              >
-                <p className="flex items-center gap-2 hover:opacity-75 hover:scale-105 transition-all ease-in-out duration-300">
-                  <Sparkles
-                    className="text-neutral fill-accent size-8"
-                    strokeWidth={1}
-                  />{" "}
-                  WriteLight
-                </p>
-              </NavLink>
-            </div>
+	return (
+		<div>
+			<header className="bg-base-200 border-b border-base-content/10">
+				<div className="mx-auto max-w-7xl p-4">
+					<div className="flex items-center justify-between">
+						{/* Logo & Site Title  */}
+						<div className="flex gap-4">
+							<NavLink
+								to={'/'}
+								className={({ isActive }) =>
+									isActive
+										? 'text-2xl text-primary bg-gradient-to-b from-primary/10 to-transparent px-6 py-2 rounded-t-xl'
+										: 'text-2xl font-semibold text-secondary px-6 py-2'
+								}
+							>
+								<div className="flex items-center gap-4 hover:opacity-75 hover:scale-105 transition-all ease-in-out duration-300">
+									<Sparkles
+										className="text-neutral fill-accent size-8"
+										strokeWidth={1}
+									/>
+									WriteLight
+								</div>
+							</NavLink>
+						</div>
 
-            {/* Journal Icon & Link */}
-            <div className="flex gap-4">
-              <NavLink
-                to={"/journal"}
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-2xl bg-gradient-to-b from-primary/10 to-transparent text-primary px-4 py-2 rounded-t-xl"
-                    : "text-2xl font-semibold text-secondary"
-                }
-              >
-                <p className="flex items-center gap-2 hover:opacity-75 hover:scale-105 transition-all ease-in-out duration-300">
-                  <NotebookPen
-                    className="text-neutral fill-accent size-8"
-                    strokeWidth={1}
-                  />{" "}
-                  Journal
-                </p>
-              </NavLink>
-            </div>
+						{/* Journal Icon & Link */}
+						<div className="flex gap-4">
+							<NavLink
+								to={'/journal'}
+								className={({ isActive }) =>
+									isActive
+										? 'text-2xl text-primary bg-gradient-to-b from-primary/10 to-transparent px-6 py-2 rounded-t-xl'
+										: 'text-2xl font-semibold text-secondary px-6 py-2'
+								}
+							>
+								<div className="flex items-center gap-4 hover:opacity-75 hover:scale-105 transition-all ease-in-out duration-300">
+									<NotebookPen
+										className="text-neutral fill-accent size-8"
+										strokeWidth={1}
+									/>
+									Journal
+								</div>
+							</NavLink>
+						</div>
 
-            {/* Market Icon & Link */}
-            <div className="flex gap-4">
-              <NavLink
-                to={"/market"}
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-2xl bg-gradient-to-b from-primary/10 to-transparent text-primary px-4 py-2 rounded-t-xl"
-                    : "text-2xl font-semibold text-secondary"
-                }
-              >
-                <p className="flex items-center gap-2 hover:opacity-75 hover:scale-105 transition-all ease-in-out duration-300">
-                  <Store
-                    className="text-neutral fill-accent size-8"
-                    strokeWidth={1}
-                  />{" "}
-                  Market
-                </p>
-              </NavLink>
-            </div>
+						{/* Market Icon & Link */}
+						<div className="flex gap-4">
+							<NavLink
+								to={'/market'}
+								className={({ isActive }) =>
+									isActive
+										? 'text-2xl text-primary bg-gradient-to-b from-primary/10 to-transparent px-6 py-2 rounded-t-xl'
+										: 'text-2xl font-semibold text-secondary px-6 py-2'
+								}
+							>
+								<div className="flex items-center gap-4 hover:opacity-75 hover:scale-105 transition-all ease-in-out duration-300">
+									<Store
+										className="text-neutral fill-accent size-8"
+										strokeWidth={1}
+									/>
+									Market
+								</div>
+							</NavLink>
+						</div>
 
-            {/* Settings Icon & Link */}
-            <div className="flex gap-4">
-              <NavLink
-                to={"/settings"}
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-2xl bg-gradient-to-b from-primary/10 to-transparent text-primary px-4 py-2 rounded-t-xl"
-                    : "text-2xl font-semibold text-secondary"
-                }
-              >
-                <p className="flex items-center gap-2 hover:opacity-75 hover:scale-105 transition-all ease-in-out duration-300">
-                  <Cog
-                    className="text-neutral fill-accent size-8"
-                    strokeWidth={1}
-                  />{" "}
-                  Settings
-                </p>
-              </NavLink>
-            </div>
+						{/* Settings Icon & Link */}
+						<div className="flex gap-4">
+							<NavLink
+								to={'/settings'}
+								className={({ isActive }) =>
+									isActive
+										? 'text-2xl text-primary bg-gradient-to-b from-primary/10 to-transparent px-6 py-2 rounded-t-xl'
+										: 'text-2xl font-semibold text-secondary px-6 py-2'
+								}
+							>
+								<div className="flex items-center gap-4 hover:opacity-75 hover:scale-105 transition-all ease-in-out duration-300">
+									<Cog
+										className="text-neutral fill-accent size-8"
+										strokeWidth={1}
+									/>
+									Settings
+								</div>
+							</NavLink>
+						</div>
 
-            {/* This button will remain conditionally not visible for now, until logout can be refactored */}
-            {false && (
-              <button
-                className="btn btn-outline btn-primary"
-                onClick={() => {
-                  toast.error("Logout not functional yet!");
-                }}
-              >
-                Logout
-              </button>
-            )}
-          </div>
-        </div>
-      </header>
-    </div>
-  );
+						{/* This button will remain conditionally not visible for now, until logout can be refactored */}
+						{false && (
+							<button
+								className="btn btn-outline btn-primary"
+								onClick={() => {
+									toast.error('Logout not functional yet!');
+								}}
+							>
+								Logout
+							</button>
+						)}
+					</div>
+				</div>
+			</header>
+		</div>
+	);
 };
 
 export default Navbar;
--- frontend/src/pages/MarketPage.jsx
+++ frontend/src/pages/MarketPage.jsx
@@ -1,45 +1,45 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
-import { useCookies } from "react-cookie";
-import api from "../lib/axios";
-import Navbar from "../components/generic/Navbar";
-import MarketItems from "../components/Market/MarketItems";
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { useCookies } from 'react-cookie';
+import api from '../lib/axios';
+import Navbar from '../components/generic/Navbar';
+import MarketItems from '../components/Market/MarketItems';
 
 const MarketPage = () => {
-  const navigate = useNavigate();
-  const [cookies, _, removeCookie] = useCookies([], {
-    doNotUpdate: false,
-  });
+	const navigate = useNavigate();
+	const [cookies, _, removeCookie] = useCookies([], {
+		doNotUpdate: false,
+	});
 
-  useEffect(() => {
-    const verifyCookie = async () => {
-      if (!cookies.token) {
-        navigate("/login");
-      }
+	useEffect(() => {
+		const verifyCookie = async () => {
+			if (!cookies.token) {
+				navigate('/login');
+			}
 
-      try {
-        const { data } = await api.post("/", {}, { withCredentials: true });
-        const { status } = data;
+			try {
+				const { data } = await api.post('/', {}, { withCredentials: true });
+				const { status } = data;
 
-        if (!status) {
-          removeCookie("token"), navigate("/login");
-        }
-      } catch (err) {
-        console.log({ err });
-      }
-    };
-    verifyCookie();
-  }, [navigate, cookies, removeCookie]);
+				if (!status) {
+					removeCookie('token'), navigate('/login');
+				}
+			} catch (err) {
+				console.log({ err });
+			}
+		};
+		verifyCookie();
+	}, [navigate, cookies, removeCookie]);
 
-  return (
-    <div className="min-h-screen">
-      <Navbar />
-      <div className="max-w-4xl mx-auto py-5">
-        <MarketItems />
-        {/* Items Shop to go here */}
-      </div>
-    </div>
-  );
+	return (
+		<div className="min-h-screen">
+			<Navbar />
+			<div className="max-w-4xl mx-auto py-5">
+				<MarketItems />
+				{/* Items Shop to go here */}
+			</div>
+		</div>
+	);
 };
 
 export default MarketPage;
