074618a5fbd36fd4a9f974e37a55facdff17a223
Avi161 <paudela@union.edu>
2025-07-17 22:55:44 -0400

    Add put product

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -1,9 +1,47 @@
 import Product from '../models/Product.js';
+import User from '../models/User.js';
 
 export async function getAllProducts(_, res) {
     try {
         const products = await Product.find();
         res.status(200).json({ products });
+    } catch (error) {
+        console.error('Error in getAllProducts controller', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export async function putProduct(req, res) {
+    try {
+        const user = await User.findById(req.id);
+        if (!user) return res.status(404).json({ message: 'User not found' });
+
+        const product = await Product.findById(req.params.product_id);
+        if (!product)
+            return res.status(400).json({ message: 'Item does not exist' });
+
+        if (user.coins < product.cost) {
+            return res.status(400).json({ message: 'Not enough coins' });
+        }
+
+        user.coins -= product.cost;
+
+        for (const tag of product.tags) {
+            if (!user.inventory[tag]) {
+                user.inventory[tag] = [];
+            }
+
+            if (!user.inventory[tag].includes(product.name)) {
+                user.inventory[tag].push(product.name);
+                user.inventory[tag].sort();
+            }
+        }
+
+        return res.status(200).json({
+            message: 'Purchase successful',
+            coins: user.coins,
+            inventory: user.inventory,
+        });
     } catch (error) {
         console.error('Error in getAllProducts controller', error);
         res.status(500).json({ message: 'Internal server error' });
--- backend/src/routes/productsRoutes.js
+++ backend/src/routes/productsRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { getAllProducts } from '../controllers/productsController.js';
+import { putProduct } from '../controllers/productsController.js';
 
 const router = express.Router();
 
 router.get('/', getAllProducts);
-
+router.put('/:product_id', putProduct);
 export default router;
