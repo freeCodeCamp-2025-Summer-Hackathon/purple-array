03fe3aa02a4353ba4217adc35db244f2103be9c0
imattking <matt@imattking.com>
2025-07-19 19:45:12 -0400

    feat [style]: add custom fonts to project and set up in index.css and tailwind.config.js

--- frontend/src/pages/JournalEntryPage.jsx
+++ frontend/src/pages/JournalEntryPage.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const JournalEntryPage = () => {
-  return (
-    <div>JournalEntryPage</div>
-  )
-}
+	return <div>JournalEntryPage</div>;
+};
 
-export default JournalEntryPage
+export default JournalEntryPage;
