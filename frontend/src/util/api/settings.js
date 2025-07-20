ae5eadccf6e2227a44a9ee5d1e44d6879a7604f3
imattking <matt@imattking.com>
2025-07-19 21:48:00 -0400

    feat: write updateSettings help function

--- frontend/src/util/api/settings.js
+++ frontend/src/util/api/settings.js
@@ -1,3 +1,5 @@
+import api from '../../lib/axios';
+
 export const fetchSettings = async () => {
 	try {
 		const response = await fetch('/settingsDummyData.json'); // placeholder for now
@@ -8,6 +10,29 @@ export const fetchSettings = async () => {
 		return data;
 	} catch (error) {
 		console.error('Error fetching settings:', error);
+		throw error;
+	}
+};
+
+export const updateSettings = async (data) => {
+	const url = 'settings';
+
+	try {
+		const response = await api.patch(
+			url,
+			{
+				timezone: data.timezone,
+				theme: data.theme,
+				font: data.font,
+				ink: data.ink,
+				parchment: data.parchment,
+			},
+			{
+				withCredentials: true,
+			}
+		);
+		return response;
+	} catch (error) {
 		throw error;
 	}
 };
