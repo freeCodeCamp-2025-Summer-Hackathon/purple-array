e0f63dc125eb34d23828251093e961f8d4eae18b
imattking <matt@imattking.com>
2025-07-22 01:57:38 -0400

    feat: add timezone dropdown on settings page

---
+++ frontend/src/util/api/timezones.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const fetchTimezones = async () => {
+	const url = `../../../public/timezones.json`;
+	try {
+		let timezonesArray = [];
+
+		const data = await axios.get(url);
+		if (data.data) {
+			for (let timezone of data.data) {
+				timezonesArray.push(timezone);
+			}
+		}
+
+		return timezonesArray;
+	} catch (error) {
+		console.log(error);
+	}
+};
