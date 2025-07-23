78bd20fdc162c16285daf6cc4a66be938a141e07
D-Johnson89 <d.l.johnson3009@gmail.com>
2025-07-22 20:58:37 -0400

    fix: add correct images to match correct market data
    Filter market items via useInventory hook. Minor cosmetic adjustments

--- frontend/src/components/Market/MarketItems.jsx
+++ frontend/src/components/Market/MarketItems.jsx
@@ -1,52 +1,38 @@
 //import { useState, useEffect } from 'react';
 import useProducts from '../../util/hooks/useProducts.js';
+import useInventory from '../../util/hooks/useInventory.js'
 import { CircleDollarSign } from 'lucide-react';
 
 function MarketItems() {
-	/**********************************************************************/
-	// use the products array being fetched in on line 7 to populate the UI
 	const { products, isLoading } = useProducts();
+    const { inventory, inventoryLoading } = useInventory();
 
-	/*
-	/*
-	// Lines 12 - 26 will be replaced with the destructured values from line 7
-	const [items, setItems] = useState([]);
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		fetch('/marketDummyData.json')
-			.then((res) => res.json())
-			.then((data) => {
-				setItems(data);
-				setLoading(false);
-			})
-			.catch((err) => {
-				console.error('Error loading JSON:', err);
-				setLoading(false);
-			});
-	}, []);
-
-    */
-	// loading here on line 30 will need to be replaced with isLoading from line 7
-	
-	if (isLoading) {
+    if (isLoading || inventoryLoading) {
 		return <p className="text-center py-10">Loading market items...</p>;
 	}
-
+    console.log(products);
+    const marketItems = products.filter( product => {
+        return !product.tags.some(tag =>
+            inventory[tag]?.includes(product.name)
+        );
+    });
+    
 	return (
 		<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-4 pt-10">
-			{products.map((item) => (
+			{marketItems.map((item) => (
+                
 				<div
-					key={item._id.$oid}
+					key={item._id}
 					className="card bg-base-100 border border-base-200 shadow-sm transition duration-300 transform hover:-translate-y-1 hover:shadow-xl hover:bg-base-200"
 				>
 					<figure>
 						<img
-							src={`https://picsum.photos/seed/${encodeURIComponent(
-								item.name
-							)}/300/200`}
+							src={`assets/${item.tags[0]}/${encodeURIComponent(
+								item.name.toLowerCase().replace(/\s+/g, '-')
+							)}.png`}
 							alt={item.name}
 							className="w-full h-48 object-cover"
+                            onError={(e) => console.log('Failed to load:', e.target.src)}
 						/>
 					</figure>
 
@@ -56,27 +42,30 @@ function MarketItems() {
 						</h2>
 						<p className="text-sm text-base-content/70">{item.description}</p>
 
-						<div className="flex items-center font-semibold text-base-content">
-							<CircleDollarSign
+						<div className="flex items-center justify-evenly font-semibold text-base-content">
+							<div className="flex items-center justify-center">
+                                <CircleDollarSign
 								className="text-violet-950 fill-yellow-500 size-7 pr-1"
 								strokeWidth={1}
 							/>
-							{item.cost.$numberInt}
-						</div>
-
-						<div className="flex flex-wrap justify-center gap-2 mt-3">
-							{item.tags.map((tag) => (
+							{item.cost}
+                            </div>
+                            {item.tags.map((tag) => (
 								<span
-									key={`${item._id.$oid}-${tag}`}
+									key={`${item._id}-${tag}`}
 									className="badge badge-outline"
 								>
 									{tag}
 								</span>
 							))}
 						</div>
+
+						<div className="flex flex-wrap justify-center gap-2 mt-3">
+							
+						</div>
 					</div>
 				</div>
-			))}
+            ))}
 		</div>
 	);
 }
--- frontend/src/util/hooks/useInventory.js
+++ frontend/src/util/hooks/useInventory.js
@@ -2,18 +2,20 @@ import { useCallback, useEffect, useState } from 'react';
 import { fetchInventory } from '../api/inventory';
 
 const useInventory = () => {
-	const [data, setData] = useState({});
-	const [isLoading, setIsLoading] = useState(false);
+	const [inventory, setInventory] = useState({});
+	const [inventoryLoading, setInventoryIsLoading] = useState(false);
+
 
 	const getInventory = useCallback(async () => {
 		try {
-			setIsLoading(true);
-			const inventory = await fetchInventory();
-			setData(inventory);
+			setInventoryIsLoading(true);
+			const data = await fetchInventory();
+			setInventory(data);
 		} catch (error) {
-			setData({});
+            console.log('Erorr getting inventory:', error);
+			setInventory({});
 		} finally {
-			setIsLoading(false);
+			setInventoryIsLoading(false);
 		}
 	}, []);
 
@@ -21,7 +23,7 @@ const useInventory = () => {
 		getInventory();
 	}, []);
 
-	return { inventory: data, isLoading };
+	return { inventory: inventory.inventory, inventoryLoading };
 };
 
 export default useInventory;
