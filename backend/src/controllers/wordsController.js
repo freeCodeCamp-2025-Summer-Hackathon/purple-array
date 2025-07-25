b00f2e97a03fc322587341cc10073929acf9d479
Damian <mystelaurora@gmail.com>
2025-07-25 12:52:10 -0700

    feat: return word based on user timezone settings

--- backend/src/controllers/wordsController.js
+++ backend/src/controllers/wordsController.js
@@ -1,5 +1,7 @@
+import User from '../models/User.js';
 import { Word } from '../models/Word.js';
 import { wordObj } from '../utils/wordHelper.js';
+import { DateTime } from 'luxon';
 
 export async function addWord(req, res) {
     try {
@@ -15,6 +17,26 @@ export async function addWord(req, res) {
 
 export async function getWord(req, res) {
     try {
+        const { settings } = await User.findById(req.id, 'settings -_id');
+        if (settings.timezone !== 'Default') {
+            const userDay = DateTime.local({ zone: settings.timezone }).day;
+            const utcDay = DateTime.utc().day;
+            if (userDay === utcDay - 1 || (utcDay === 1 && userDay > 2)) {
+                const { word, pronunciation, definition } =
+                    wordObj.previousWord;
+                return res
+                    .status(200)
+                    .json({ word, pronunciation, definition });
+            } else if (
+                userDay === utcDay + 1 ||
+                (utcDay === 1 && userDay > 2)
+            ) {
+                const { word, pronunciation, definition } = wordObj.nextWord;
+                return res
+                    .status(200)
+                    .json({ word, pronunciation, definition });
+            }
+        }
         const { word, pronunciation, definition } = wordObj.currentWord;
         res.status(200).json({ word, pronunciation, definition });
     } catch (error) {
