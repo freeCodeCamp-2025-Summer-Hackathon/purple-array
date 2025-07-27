480c3a4b6c76ba383c74b042024e4a7dc1e9a7dd
imattking <matt@imattking.com>
2025-07-27 15:30:46 -0400

    Bug [Fix]: add logut button to journal collection page

--- frontend/src/pages/JournalCollectionPage.jsx
+++ frontend/src/pages/JournalCollectionPage.jsx
@@ -7,12 +7,12 @@ const JournalCollectionPage = () => {
 
 	return (
 		<div className="min-h-screen">
-			<Navbar logout={handleLogout} />
+			<Navbar logout={handleLogout} cookies={cookies} />
 			<div className="max-w-4xl mx-auto py-20">
 				<h1 className="text-center text-3xl font-semibold text-secondary">
 					Journal Collection
 				</h1>
-				{/*Journal collection Components*/}
+
 				<JournalEntriesList />
 			</div>
 		</div>
