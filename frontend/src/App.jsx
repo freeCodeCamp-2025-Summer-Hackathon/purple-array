eae206bdefaa2fbf7d03de46c9f2db26088eef02
imattking <matt@imattking.com>
2025-07-26 22:08:14 -0400

    remove logs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,55 @@
-import { Routes, Route } from "react-router";
-import { useEffect, useState } from "react";
-import useSettings from "./util/hooks/useSettings";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import JournalDisplayPage from "./pages/JournalDisplayPage";
-import JournalCollectionPage from "./pages/JournalCollectionPage";
-import JournalEntryPage from "./pages/JournalEntryPage";
-import JournalEditPage from "./pages/JournalEditPage";
-import MarketPage from "./pages/MarketPage";
-import SettingsPage from "./pages/SettingsPage";
+import { Routes, Route } from 'react-router';
+import { useEffect, useState } from 'react';
+import useSettings from './util/hooks/useSettings';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import JournalDisplayPage from './pages/JournalDisplayPage';
+import JournalCollectionPage from './pages/JournalCollectionPage';
+import JournalEntryPage from './pages/JournalEntryPage';
+import JournalEditPage from './pages/JournalEditPage';
+import MarketPage from './pages/MarketPage';
+import SettingsPage from './pages/SettingsPage';
 
 const App = () => {
-  const settings = useSettings();
-  const [theme, setTheme] = useState();
+	const settings = useSettings();
+	const [theme, setTheme] = useState();
 
-  useEffect(() => {
-    const useTheme = async () => {
-      const theme = await settings.settings.theme.toLowerCase();
-      console.log(theme);
-      if (theme) {
-        setTheme(theme);
-      }
-    };
+	useEffect(() => {
+		const useTheme = async () => {
+			const theme = await settings.settings.theme.toLowerCase();
 
-    useTheme();
-  }, [settings]);
+			if (theme) {
+				setTheme(theme);
+			}
+		};
 
-  return (
-    theme && (
-      <div data-theme={theme}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/journal" element={<JournalDisplayPage />} />
-          <Route
-            path="/journal/collection"
-            element={<JournalCollectionPage />}
-          />
-          <Route
-            path="/journal/collection/:id"
-            element={<JournalEntryPage />}
-          />
-          <Route path="/edit/:id" element={<JournalEditPage />} />
-          <Route path="/market" element={<MarketPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-        </Routes>
-      </div>
-    )
-  );
+		useTheme();
+	}, [settings]);
+
+	return (
+		theme && (
+			<div data-theme={theme}>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/signup" element={<SignupPage />} />
+					<Route path="/journal" element={<JournalDisplayPage />} />
+					<Route
+						path="/journal/collection"
+						element={<JournalCollectionPage />}
+					/>
+					<Route
+						path="/journal/collection/:id"
+						element={<JournalEntryPage />}
+					/>
+					<Route path="/edit/:id" element={<JournalEditPage />} />
+					<Route path="/market" element={<MarketPage />} />
+					<Route path="/settings" element={<SettingsPage />} />
+				</Routes>
+			</div>
+		)
+	);
 };
 
 export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,68 +1,66 @@
-import typography from "@tailwindcss/typography";
-import daisyui from "daisyui";
+import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  important: true,
-  theme: {
-    extend: {
-      fontFamily: {
-        caveat: ["Caveat"], //handwritten, written as font-caveat
-        fred: ['"Fredericka the Great"'], //chalk, written as font-fred
-        league: ['"League Script"'], // cursive, written as font-league
-        type: ['"Special Elite"'], // typewriter, written as font-type
-      },
-      animation: {
-        "spin-slow": "spin 3s linear infinite",
-      },
-    },
-  },
-  plugins: [typography, daisyui],
-  daisyui: {
-    themes: [
-      "cupcake",
-      "valentine",
-      "garden",
-      "pastel",
-      "dracula",
-      "night",
-      "coffee",
-      "winter",
-      "sunset",
-      "luxury",
-      "forest",
-      "aqua",
-      "fantasy",
-      "cmyk",
-      "autumn",
-      "synthwave",
-
-      {
-        default: {
-          primary: "#463aa2",
-          secondary: "#4b4b4b",
-          accent: "#8f97ff",
-          neutral: "221551",
-          "base-100": "#ffffff",
-          info: "#8fd6ff",
-          success: "#96f2b9",
-          warning: "#fdff8f",
-          error: "#ff7a7a",
-        },
-
-        wireframe: {
-          primary: "#757575",
-          secondary: "#7a7a7a",
-          accent: "#eeeeee",
-          neutral: "#757575",
-          "base-100": "#ffffff",
-          info: "#8fd6ff",
-          success: "#96f2b9",
-          warning: "#fdff8f",
-          error: "#ff7a7a",
-        },
-      },
-    ],
-  },
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	important: true,
+	theme: {
+		extend: {
+			fontFamily: {
+				caveat: ['Caveat'], //handwritten, written as font-caveat
+				fred: ['"Fredericka the Great"'], //chalk, written as font-fred
+				league: ['"League Script"'], // cursive, written as font-league
+				type: ['"Special Elite"'], // typewriter, written as font-type
+			},
+			animation: {
+				'spin-slow': 'spin 3s linear infinite',
+			},
+		},
+	},
+	plugins: [typography, daisyui],
+	daisyui: {
+		themes: [
+			'cupcake',
+			'valentine',
+			'garden',
+			'pastel',
+			'dracula',
+			'night',
+			'coffee',
+			'winter',
+			'sunset',
+			'luxury',
+			'forest',
+			'aqua',
+			'fantasy',
+			'cmyk',
+			'autumn',
+			'synthwave',
+			{
+				default: {
+					primary: '#463aa2',
+					secondary: '#4b4b4b',
+					accent: '#8f97ff',
+					neutral: '221551',
+					'base-100': '#ffffff',
+					info: '#8fd6ff',
+					success: '#96f2b9',
+					warning: '#fdff8f',
+					error: '#ff7a7a',
+				},
+				wireframe: {
+					primary: '#757575',
+					secondary: '#7a7a7a',
+					accent: '#eeeeee',
+					neutral: '#757575',
+					'base-100': '#ffffff',
+					info: '#8fd6ff',
+					success: '#96f2b9',
+					warning: '#fdff8f',
+					error: '#ff7a7a',
+				},
+			},
+		],
+	},
 };
